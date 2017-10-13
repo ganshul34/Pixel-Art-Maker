844e04252ef72f62d1cfc17f3afa654fce60bad6
Anshul Gupta <anshul1144.cse16@chitkara.edu.in>
2017-10-13 22:04:03 +0530

    Updated JS code

--- designs.js
+++ designs.js
@@ -1,10 +1,35 @@
 // Select color input
+let color = $("#colorPicker");
+
 // Select size input
+var height = $("#input_height");
+var width = $("#input_width");
+
+const table = document.getElementById('pixel_canvas');
+
+$("#input_submit").click(function (e) {
+    e.preventDefault();
+    makeGrid();
+});
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
 
-// Your code goes here!
+    table.innerHTML = '';
+    let h = height.val();
+    let w = width.val();
 
+    let addColor = function (cell) {
+        cell.addEventListener('click', function () {
+            cell.style.backgroundColor = color.val();
+        });
+    }
+
+    for (let i = 0; i < h; i++) {
+        let row = table.insertRow(i);
+        for (var j = 0; j < w; j++) {
+            let cell = row.insertCell(j);
+            cell.addEventListener('click', addColor(cell));
+        }
+    }
 }
